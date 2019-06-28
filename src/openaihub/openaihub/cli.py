060f919edac4299fb4591a648799721e50ba8af4
Weiqiang Zhuang <wzhuang@Weiqiangs-MacBook-Pro.local>
2019-06-28 12:23:44 -0700

    update wait

--- src/openaihub/openaihub/cli.py
+++ src/openaihub/openaihub/cli.py
@@ -12,8 +12,12 @@ logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 def run(cmd):
     ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    logger.info("command = %s, returncode = %s" % (ret.args, ret.returncode))
+    logger.info("command: %s, returncode: %s" % (ret.args, ret.returncode))
     return(ret)
+
+def wait_for(operator, namespace):
+    while run("kubectl rollout status deployment/%s-operator -n %s" % (operator, namespace)).returncode != 0 :
+        time.sleep(30)
 
 import click
 from click import UsageError
@@ -37,7 +41,7 @@ def install(namespace, version):
 
     # prereq: helm must be installed already
     # init helm tiller service account
-    logger.info("1/10 Init helm tiller...")
+    logger.info("### 1/13 ### Init helm tiller...")
     run("kubectl create -f %s/src/requirement/helm-tiller.yaml" % basedir)
     run("helm init --service-account tiller --upgrade")
 
@@ -47,49 +51,61 @@ def install(namespace, version):
     openaihub_cr_path = "%s/src/registry/cr_samples" % basedir
 
     # install OLM
-    logger.info("2/10 Install OLM...")
+    logger.info("### 2/13 ### Install OLM...")
     olm_version = "0.10.0"
     import wget
     wget.download("https://github.com/operator-framework/operator-lifecycle-manager/releases/download/%s/install.sh" % olm_version, out="%s/install.sh" % basedir)
     run("bash %s/install.sh %s" % (basedir, olm_version))
 
     # add openaihub catalog
-    logger.info("3/10 Add OpenAIHub operators catalog...")
+    logger.info("### 3/13 ### Add OpenAIHub operators catalog...")
     run("kubectl apply -f %s/openaihub.catalogsource.yaml" % openaihub_catalog_path)
 
     # create kubeflow namespace
     run("kubectl create namespace kubeflow")
 
     # create jupyterlab operator
-    logger.info("4/10 Deploy Jupyterlab operator...")
+    logger.info("### 4/13 ### Deploy Jupyterlab operator...")
     run("kubectl apply -f %s/%s-operator.yaml" % (openaihub_subscription_path, "jupyterlab"))
 
+    # wait until jupyterlab operator is available
+    logger.info("### 5/13 ### Wait until Jupyterlab operator is available...")
+    wait_for("jupyterlab", openaihub_namespace)
+
     # create jupyterlab cr
-    logger.info("5/10 Create Jupyterlab deployment...")
+    logger.info("### 6/13 ### Create Jupyterlab deployment...")
     run("kubectl apply -f %s/openaihub_v1alpha1_%s_cr.yaml -n %s" % (openaihub_cr_path, "jupyterlab", openaihub_namespace))
 
     # switch default storageclass to nfs-dynamic
-    # TBD: add an exit condition
-    logger.info("6/10 Wait for nfs-dynamic storageclass to be ready and set as default...")
+    # TBD: add a timeout exit condition
+    logger.info("### 7/13 ### Wait for nfs-dynamic storageclass to be ready and set as default...")
     while (run("kubectl get storageclass |grep nfs-dynamic").stdout.decode() == ''):
-        time.sleep(10)
+        time.sleep(30)
     run("kubectl patch storageclass ibmc-file-bronze -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"false\"}}}'")
     run("kubectl patch storageclass nfs-dynamic -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}'")
 
     # create pipelines operator
-    logger.info("7/10 Deploy Pipelines operator...")
+    logger.info("### 8/13 ### Deploy Pipelines operator...")
     run("kubectl apply -f %s/%s-operator.yaml" % (openaihub_subscription_path, "pipelines"))
 
+    # wait until pipelines operator is available
+    logger.info("### 9/13 ### Wait until Pipelines operator is available...")
+    wait_for("pipelines", openaihub_namespace)
+
     # create pipelines cr
-    logger.info("8/10 Create Pipelines deployment...")
+    logger.info("### 10/13 ### Create Pipelines deployment...")
     run("kubectl apply -f %s/openaihub_v1alpha1_%s_cr.yaml -n %s" % (openaihub_cr_path, "pipelines", openaihub_namespace))
 
     # create openaihub operator
-    logger.info("9/10 Deploy OpenAIHub operator...")
+    logger.info("### 11/13 ### Deploy OpenAIHub operator...")
     run("kubectl apply -f %s/%s-operator.yaml" % (openaihub_subscription_path, "openaihub"))
 
+    # wait until openaihub operator is available
+    logger.info("### 12/13 ### Wait until OpenAIHub operator is available...")
+    wait_for("openaihub", openaihub_namespace)
+
     # create openaihub cr
-    logger.info("10/10 Create OpenAIHub deployment...")
+    logger.info("### 13/13 ### Create OpenAIHub deployment...")
     run("kubectl apply -f %s/openaihub_v1alpha1_%s_cr.yaml -n %s" % (openaihub_cr_path, "openaihub", openaihub_namespace))
 
     logger.info("Done.")
