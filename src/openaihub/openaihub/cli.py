c4042615df792e37217a437c071e15f60fb55636
Weiqiang Zhuang <wzhuang@weiqiangs-mbp.usca.ibm.com>
2019-07-03 14:45:42 -0700

    ready

--- src/openaihub/openaihub/cli.py
+++ src/openaihub/openaihub/cli.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import logging
 import sys
+import platform
 import subprocess
 import time
 from git import Repo
@@ -42,34 +43,59 @@ def cli():
 @click.option("--version", "-v", metavar="VERSION",
               help="")
 def register(path, operator, version):
+    logger.addHandler(logging.FileHandler(os.path.join(path, "openaihub.log")))
+
     # unpack operator tgz files to src/registry/operators
+    logger.info("### 1/8 ### Unpack operator tgz...")
     kaniko_path = os.path.join(basedir, "src/registry/kaniko")
     operator_path = os.path.join(kaniko_path, "operators", operator)
-    os.mkdir(operator_path)
+    os.makedirs(operator_path, exist_ok=True)
     tf = tarfile.open(os.path.join(path, operator + ".tgz"), "r:gz")
     tf.extractall(operator_path)
 
     # create context.tgz
+    logger.info("### 2/8 ### Create build-context...")
     kaniko_tgz = os.path.join(basedir, "kaniko.tgz")
     tf = tarfile.open(kaniko_tgz, "w:gz")
-    tf.add(kaniko_path, arcname="")
+    tf.add(os.path.join(kaniko_path, "/Dockerfile"), arcname="Dockerfile")
+    tf.add(os.path.join(kaniko_path, "/operators"), arcname="operators")
     tf.close()
 
     # create docker-config ConfigMap
-    run("kubectl create ConfigMap docker-config --from-file=%s/config.json" % kaniko_path)
+    logger.info("### 3/8 ### Create docker config...")
+    run("kubectl create configmap docker-config --from-file=%s/config.json" % kaniko_path)
 
     # modify kaniko.yaml with operator destination
-    run("sed -i 's/destination=.*$/destination=abc:v0.0.1\"/' %s/kaniko.yaml" % kaniko_path)
+    logger.info("### 4/8 ### Create kaniko pod...")
+    run("sed -i %s 's/destination=.*$/destination=docker.io\/ffdlops\/%s-catalog:v0.0.1\"/' %s/kaniko.yaml" % ("''" if platform.system() == 'Darwin' else '', operator, kaniko_path))
 
     # create kaniko pod
     run("kubectl apply -f %s/kaniko.yaml" % kaniko_path)
 
     # copy build context to kaniko container
+    logger.info("### 5/8 ### Set up kaniko job...")
     run("kubectl cp %s kaniko:/tmp/context.tar.gz -c kaniko-init" % kaniko_tgz)
     run("kubectl exec kaniko -c kaniko-init -- tar -zxf /tmp/context.tar.gz -C /kaniko/build-context")
     run("kubectl exec kaniko -c kaniko-init -- touch /tmp/complete")
 
     # now wait for the image to be built and ready
+    logger.info("### 6/8 ### Wait for the image to be ready...")
+    while run("kubectl get pod/kaniko|grep kaniko|awk '{ print $3;exit}'").stdout.decode() != "Completed\n" :
+        time.sleep(30)
+
+    # delete the kaniko pod
+    logger.info("### 7/8 ### Delete the kaniko pod...")
+    run("kubectl delete -f %s/kaniko.yaml" % kaniko_path)
+
+    # generate catalog source yaml
+    logger.info("### 8/8 ### Deploy the catalog...")
+    run("sed -i %s 's/REPLACE_OPERATOR/%s/' %s/catalogsource.yaml" % ("''" if platform.system() == 'Darwin' else '', operator, kaniko_path))
+    run("sed -i %s 's/REPLACE_IMAGE/docker.io\/ffdlops\/%s-catalog:v0.0.1/' %s/catalogsource.yaml" % ("''" if platform.system() == 'Darwin' else '', operator, kaniko_path))
+
+    # deploy the catalog
+    run("kubectl apply -f %s/catalogsource.yaml" % kaniko_path)
+
+    logger.info("Done.")
     
 @cli.command()
 @click.option("--namespace", "-e", metavar="NAME", default="default",
