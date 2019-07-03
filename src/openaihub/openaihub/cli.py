b0f2a4c77cd9c47d2183382796090db46aa50f97
Weiqiang Zhuang <wzhuang@weiqiangs-mbp.usca.ibm.com>
2019-07-03 16:17:02 -0700

    update

--- src/openaihub/openaihub/cli.py
+++ src/openaihub/openaihub/cli.py
@@ -57,8 +57,8 @@ def register(path, operator, version):
     logger.info("### 2/8 ### Create build-context...")
     kaniko_tgz = os.path.join(basedir, "kaniko.tgz")
     tf = tarfile.open(kaniko_tgz, "w:gz")
-    tf.add(os.path.join(kaniko_path, "/Dockerfile"), arcname="Dockerfile")
-    tf.add(os.path.join(kaniko_path, "/operators"), arcname="operators")
+    tf.add(os.path.join(kaniko_path, "Dockerfile"), arcname="Dockerfile")
+    tf.add(os.path.join(kaniko_path, "operators"), arcname="operators")
     tf.close()
 
     # create docker-config ConfigMap
@@ -71,6 +71,9 @@ def register(path, operator, version):
 
     # create kaniko pod
     run("kubectl apply -f %s/kaniko.yaml" % kaniko_path)
+
+    # wait for the pod to be ready
+    time.sleep(60)
 
     # copy build context to kaniko container
     logger.info("### 5/8 ### Set up kaniko job...")
@@ -103,9 +106,11 @@ def register(path, operator, version):
 @click.option("--version", "-v", metavar="VERSION",
               help="")
 def install(namespace, version):
+    steps = 14
+
     # prereq: helm must be installed already
     # init helm tiller service account
-    logger.info("### 1/13 ### Init helm tiller...")
+    logger.info("### 1/%s ### Init helm tiller..." % steps)
     run("kubectl create -f %s/src/requirement/helm-tiller.yaml" % basedir)
     run("helm init --service-account tiller --upgrade")
 
@@ -115,61 +120,65 @@ def install(namespace, version):
     openaihub_cr_path = "%s/src/registry/cr_samples" % basedir
 
     # install OLM
-    logger.info("### 2/13 ### Install OLM...")
+    logger.info("### 2/%s ### Install OLM..." % steps)
     olm_version = "0.10.0"
     import wget
     wget.download("https://github.com/operator-framework/operator-lifecycle-manager/releases/download/%s/install.sh" % olm_version, out="%s/install.sh" % basedir)
     run("bash %s/install.sh %s" % (basedir, olm_version))
 
     # add openaihub catalog
-    logger.info("### 3/13 ### Add OpenAIHub operators catalog...")
+    logger.info("### 3/%s ### Add OpenAIHub operators catalog..." % steps)
     run("kubectl apply -f %s/openaihub.catalogsource.yaml" % openaihub_catalog_path)
 
     # create kubeflow namespace
     run("kubectl create namespace kubeflow")
 
     # create jupyterlab operator
-    logger.info("### 4/13 ### Deploy Jupyterlab operator...")
+    logger.info("### 4/%s ### Deploy Jupyterlab operator..." % steps)
     run("kubectl apply -f %s/%s-operator.yaml" % (openaihub_subscription_path, "jupyterlab"))
 
     # wait until jupyterlab operator is available
-    logger.info("### 5/13 ### Wait until Jupyterlab operator is available...")
+    logger.info("### 5/%s ### Wait until Jupyterlab operator is available..." % steps)
     wait_for("jupyterlab", openaihub_namespace)
 
     # create jupyterlab cr
-    logger.info("### 6/13 ### Create Jupyterlab deployment...")
+    logger.info("### 6/%s ### Create Jupyterlab deployment..." % steps)
     run("kubectl apply -f %s/openaihub_v1alpha1_%s_cr.yaml -n %s" % (openaihub_cr_path, "jupyterlab", openaihub_namespace))
 
     # switch default storageclass to nfs-dynamic
     # TBD: add a timeout exit condition
-    logger.info("### 7/13 ### Wait for nfs-dynamic storageclass to be ready and set as default...")
+    logger.info("### 7/%s ### Wait for nfs-dynamic storageclass to be ready and set as default..." % steps)
     while (run("kubectl get storageclass |grep nfs-dynamic").stdout.decode() == ''):
         time.sleep(30)
     run("kubectl patch storageclass ibmc-file-bronze -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"false\"}}}'")
     run("kubectl patch storageclass nfs-dynamic -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}'")
 
     # create pipelines operator
-    logger.info("### 8/13 ### Deploy Pipelines operator...")
+    logger.info("### 8/%s ### Deploy Pipelines operator..." % steps)
     run("kubectl apply -f %s/%s-operator.yaml" % (openaihub_subscription_path, "pipelines"))
 
     # wait until pipelines operator is available
-    logger.info("### 9/13 ### Wait until Pipelines operator is available...")
+    logger.info("### 9/%s ### Wait until Pipelines operator is available..." % steps)
     wait_for("pipelines", openaihub_namespace)
 
     # create pipelines cr
-    logger.info("### 10/13 ### Create Pipelines deployment...")
+    logger.info("### 10/%s ### Create Pipelines deployment..." % steps)
     run("kubectl apply -f %s/openaihub_v1alpha1_%s_cr.yaml -n %s" % (openaihub_cr_path, "pipelines", openaihub_namespace))
 
     # create openaihub operator
-    logger.info("### 11/13 ### Deploy OpenAIHub operator...")
+    logger.info("### 11/%s ### Deploy OpenAIHub operator..." % steps)
     run("kubectl apply -f %s/%s-operator.yaml" % (openaihub_subscription_path, "openaihub"))
 
     # wait until openaihub operator is available
-    logger.info("### 12/13 ### Wait until OpenAIHub operator is available...")
+    logger.info("### 12/%s ### Wait until OpenAIHub operator is available..." % steps)
     wait_for("openaihub", openaihub_namespace)
 
     # create openaihub cr
-    logger.info("### 13/13 ### Create OpenAIHub deployment...")
+    logger.info("### 13/%s ### Create OpenAIHub deployment..." % steps)
     run("kubectl apply -f %s/openaihub_v1alpha1_%s_cr.yaml -n %s" % (openaihub_cr_path, "openaihub", openaihub_namespace))
+
+    # add cluster-admin to default service account for registration and installation of other operators
+    logger.info("### 14/%s ### Add cluster admin..." % steps)
+    run("kubectl create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=%s:default" % openaihub_namespace)
 
     logger.info("Done.")
